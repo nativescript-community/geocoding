cb10d51957a854b5cc07077d5094447320e7599d
Tim Doege <tim.doege@solteq.com>
2019-03-12 21:57:55 +0100

    feat: add vanilla Nativescript demo and webpack support

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,32 +1,39 @@
-/*
-In NativeScript, a file with the same name as an XML file is known as
-a code-behind file. The code-behind is a great place to place your view
-logic, and to set up your page’s data binding.
-*/
-
-import { EventData } from "tns-core-modules/data/observable";
+import { Observable } from "tns-core-modules/data/observable";
+import { ObservableArray } from "tns-core-modules/data/observable-array";
 import { Page } from "tns-core-modules/ui/page";
-import { HelloWorldModel } from "./main-view-model";
-import * as geocoding from "nativescript-geocoding";
+import { SearchBar } from "tns-core-modules/ui/search-bar";
+import * as geocoding from 'nativescript-geocoding';
 
-// Event handler for Page "navigatingTo" event attached in main-page.xml
-export function navigatingTo(args: EventData) {
-    /*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
-    const page = <Page>args.object;
+export function onNavigatingTo(args) {
+    const page: Page = <Page>args.object;
+    const vm = new Observable();
+    const location = new geocoding.Location();
+    vm.set("location", location);
+    page.bindingContext = vm;
+}
+// search for country
+export function onSubmit(args) {
+    const searchBar: SearchBar = <SearchBar>args.object;
+    const searchValue = searchBar.text.toLowerCase();
+    const page: Page = <Page>searchBar.page;
+    const vm = page.bindingContext;
 
-    /*
-    A page’s bindingContext is an object that should be used to perform
-    data binding between XML markup and TypeScript code. Properties
-    on the bindingContext can be accessed using the {{ }} syntax in XML.
-    In this example, the {{ message }} and {{ onTap }} bindings are resolved
-    against the object returned by createViewModel().
-
-    You can learn more about data binding in NativeScript at
-    https://docs.nativescript.org/core-concepts/data-binding.
-    */
-    page.bindingContext = new HelloWorldModel();
+    const myItems = new ObservableArray();
+    if (searchValue !== "") {
+        geocoding.getLocationFromName(searchValue).then(loc => {
+            vm.set("location", loc);
+        }, function (e) {
+            console.log("Error: " + (e.message || e));
+        });
+    }
+}
+// clear SearchBar text and load ListView initial data
+export function onClear(args) {
+    const searchBar: SearchBar = <SearchBar>args.object;
+    searchBar.text = "";
+    searchBar.hint = "Location search string";
+    const location = new geocoding.Location();
+    const page: Page = <Page>searchBar.page;
+    const vm = page.bindingContext;
+    vm.set("location", location);
 }
